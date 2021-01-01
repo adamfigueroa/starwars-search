43767d4b05cef27883301b16328c0606720b218c
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-31 17:38:37 -0800

    added test and final commit

--- src/components/SearchForm.test.js
+++ src/components/SearchForm.test.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import SearchForm from "./SearchForm";
 import renderer from "react-test-renderer";
 
-describe("App Component", () => {
+describe("SearchForm Component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<SearchForm />, div);
 
     ReactDOM.unmountComponentAtNode(div);
   });
-
   it("renders the UI", () => {
-    const tree = renderer.create(<App />).toJSON();
+    const tree = renderer.create(<SearchForm />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
