01b5eb5a51cf337320d49674a509519fcfa7b692
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-31 17:28:58 -0800

    added test and final commit

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import renderer from "react-test-renderer";
+
+describe("App Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("render the UI", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
